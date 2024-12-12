375e90b5f0a1df20e0e631951cd24037fc869e88
Vanpa99 <mativanpa99@gmail.com>
2024-12-12 20:24:26 -0300

    mas de css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,23 +3,59 @@ import { useNavigate } from "react-router-dom";
 import Prestamos from "./Prestamos";
 import Sucursales from "./Sucursales";
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+
+
 function Home() {
 
   return (
     <>
-      <h1>HOME</h1>
-      {/* <Prestamos /> */}
+      <h2 className={styles.sectionTitle}>HOME</h2>
 
-      <ul>
-          <li> <Link to="/mis-datos">Consultar tus datos personales</Link> (Clientes autenticados).</li>
-          <li>Obtener saldo de tus cuentas bancarias (Clientes autenticados).</li>
-          <li>Consultar el monto total de tus préstamos (Clientes autenticados).</li>
-          <li>Listar préstamos por sucursal (Empleados autenticados).</li>
-          <li>Listar las tarjetas de crédito asociadas a un cliente (Empleados autenticados).</li>
-          <li>Generar solicitudes de préstamos para clientes (Empleados autenticados).</li>
-          <li>Anular solicitudes de préstamos de clientes (Empleados autenticados).</li>
-          <li>Modificar la dirección de un cliente (Clientes autenticados y empleados).</li>
-          <li> <Link to="/sucursales">Consultar el listado de todas las sucursales </Link> (Público).</li>
+      <ul className={styles.serviceList}>
+        <Link to="/mis-datos">
+          <li>
+            Consultar tus datos personales (Clientes autenticados).
+          </li>
+        </Link>
+
+          <li>
+            Obtener saldo de tus cuentas bancarias (Clientes autenticados).
+          </li>
+
+          <li>
+            Consultar el monto total de tus préstamos (Clientes autenticados).
+          </li>
+
+          <Link to="/sucursales">
+            <li>
+              Listar préstamos por sucursal (Empleados autenticados).
+            </li>
+          </Link>
+
+          <li>
+            Listar las tarjetas de crédito asociadas a un cliente (Empleados autenticados).
+          </li>
+
+          <li>
+            Generar solicitudes de préstamos para clientes (Empleados autenticados).
+          </li>
+          
+          <li>
+            Anular solicitudes de préstamos de clientes (Empleados autenticados).
+          </li>
+
+          <Link to="/mis-datos">
+            <li>
+              Modificar la dirección de un cliente (Clientes autenticados y empleados).
+            </li>
+          </Link>
+
+          <Link to="/sucursales">
+            <li>
+              Consultar el listado de todas las sucursales (Público).
+            </li>
+          </Link>
       </ul>
     </>
   );
